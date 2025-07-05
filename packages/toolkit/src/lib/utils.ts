1d1eeadf153abddc2beaeb5054c03798d83d67b2
ISON <azzgo@users.noreply.github.com>
2025-07-06 00:42:08 +0800

    fix: diff logic apply to nest array

--- packages/toolkit/src/lib/utils.ts
+++ packages/toolkit/src/lib/utils.ts
@@ -1,6 +1,6 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
-import {Content, JSONContent, TextContent} from "vanilla-jsoneditor";
+import { Content, JSONContent, TextContent } from "vanilla-jsoneditor";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -23,52 +23,90 @@ export const getEditorContentJson = (content: Content): unknown | undefined => {
     schema = parseJSON((content as TextContent).text);
   }
   return schema;
-}
+};
 
-export const convertDiffResultToJSONPathMap = (jsonResult: Record<string, any>) => {
-  const leftDiffResultMap = new Map<string, 'add' | 'delete' | 'update'>();
-  const rightDiffResultMap = new Map<string, 'add' | 'delete' | 'update'>();
+type WalkTarget = Record<string, unknown> | Array<unknown>;
 
-  const walk = (obj: Record<string, any>, path: string[] = []) => {
-    for (const [key, value] of Object.entries(obj)) {
-      if (key.endsWith('__added')) {
-        const jsonPath = [...path, key.replace(/__added$/, '')].filter(Boolean).join('.');
-        rightDiffResultMap.set(jsonPath, 'add');
-      } else if (key.endsWith('__deleted')) {
-        const jsonPath = [...path, key.replace(/__deleted$/, '')].filter(Boolean).join('.');
-        leftDiffResultMap.set(jsonPath, 'delete');
-      } else if (Array.isArray(value)) {
-        let leftIndex = 0;
-        let rightIndex = 0;
-        for (let entry of value) {
-          if (Array.isArray(entry)) {
-            const [op] = entry;
-            if (op === '-') {
-              const jsonPath = [...path, key, leftIndex.toString()].join('.');
-              leftIndex++;
-              leftDiffResultMap.set(jsonPath, 'delete');
-            } else if (op === '+') {
-              const jsonPath = [...path, key, rightIndex.toString()].join('.');
-              rightIndex++;
-              rightDiffResultMap.set(jsonPath, 'add');
-            } else if (op === '~') {
-              walk(entry[1], [...path, key, leftIndex.toString()]);
-              leftIndex++;
-              rightIndex++;
-            } else {
-              leftIndex++;
-              rightIndex++;
+export const convertDiffResultToJSONPathMap = (
+  jsonResult: Record<string, unknown>,
+) => {
+  const leftDiffResultMap = new Map<string, "add" | "delete" | "update">();
+  const rightDiffResultMap = new Map<string, "add" | "delete" | "update">();
+
+  const walk = (obj: WalkTarget, path: string[] = []) => {
+    if (typeof obj === "object" && obj !== null) {
+      for (const [key, value] of Object.entries(obj)) {
+        if (key.endsWith("__added")) {
+          const jsonPath = [...path, key.replace(/__added$/, "")]
+            .filter(Boolean)
+            .join(".");
+          rightDiffResultMap.set(jsonPath, "add");
+        } else if (key.endsWith("__deleted")) {
+          const jsonPath = [...path, key.replace(/__deleted$/, "")]
+            .filter(Boolean)
+            .join(".");
+          leftDiffResultMap.set(jsonPath, "delete");
+        } else if (Array.isArray(value)) {
+          let leftIndex = 0;
+          let rightIndex = 0;
+          for (const entry of value) {
+            if (Array.isArray(entry)) {
+              const [op] = entry;
+              if (op === "-") {
+                const jsonPath = [...path, key, leftIndex.toString()].join(".");
+                leftIndex++;
+                leftDiffResultMap.set(jsonPath, "delete");
+              } else if (op === "+") {
+                const jsonPath = [...path, key, rightIndex.toString()].join(
+                  ".",
+                );
+                rightIndex++;
+                rightDiffResultMap.set(jsonPath, "add");
+              } else if (op === "~") {
+                walk(entry[1], [...path, key, leftIndex.toString()]);
+                leftIndex++;
+                rightIndex++;
+              } else {
+                leftIndex++;
+                rightIndex++;
+              }
             }
           }
+        } else if (typeof value === "object" && value !== null) {
+          if ("__new" in value && "__old" in value) {
+            const jsonLeftPath = [...path, key].filter(Boolean).join(".");
+            const jsonRightPath = [...path, key].filter(Boolean).join(".");
+            leftDiffResultMap.set(jsonLeftPath, "update");
+            rightDiffResultMap.set(jsonRightPath, "update");
+          } else {
+            walk(value as WalkTarget, [...path, key]);
+          }
         }
-      } else if (typeof value === 'object' && value !== null) {
-        if (value.__new && value.__old) {
-          const jsonLeftPath = [...path, key].filter(Boolean).join('.');
-          const jsonRightPath = [...path, key].filter(Boolean).join('.');
-          leftDiffResultMap.set(jsonLeftPath, 'update');
-          rightDiffResultMap.set(jsonRightPath, 'update');
-        } else {
-          walk(value, [...path, key]);
+      }
+    }
+
+    if (Array.isArray(obj)) {
+      let leftIndex = 0;
+      let rightIndex = 0;
+      for (const entry of obj) {
+        if (Array.isArray(entry)) {
+          const [op] = entry;
+          if (op === "-") {
+            const jsonPath = [...path, leftIndex.toString()].join(".");
+            leftIndex++;
+            leftDiffResultMap.set(jsonPath, "delete");
+          } else if (op === "+") {
+            const jsonPath = [...path, rightIndex.toString()].join(".");
+            rightIndex++;
+            rightDiffResultMap.set(jsonPath, "add");
+          } else if (op === "~") {
+            walk(entry[1], [...path, leftIndex.toString()]);
+            leftIndex++;
+            rightIndex++;
+          } else {
+            leftIndex++;
+            rightIndex++;
+          }
         }
       }
     }
@@ -76,4 +114,4 @@ export const convertDiffResultToJSONPathMap = (jsonResult: Record<string, any>) 
 
   walk(jsonResult);
   return [leftDiffResultMap, rightDiffResultMap];
-}
+};
