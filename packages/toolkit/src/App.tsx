412780a97598261ddafefcb1aacc9ba9c4a7dae9
ISON <azzgo@users.noreply.github.com>
2025-07-06 10:00:04 +0800

    feat: get it work on build extension

--- packages/toolkit/src/App.tsx
+++ packages/toolkit/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router";
+import { HashRouter as Router, Route, Routes } from "react-router";
 import JsonEditor from "./pages/JsonEditor";
 import { Toaster } from "./components/ui/sonner";
 import Layout from "./components/layout";
--- packages/toolkit/vite.config.ts
+++ packages/toolkit/vite.config.ts
@@ -3,11 +3,17 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
+const buildForExtension = process.env.BUILD_FOR_EXTENSION === "1";
+
 // https://vite.dev/config/
 export default defineConfig({
+  base: buildForExtension ? "/toolkit/" : "/",
   plugins: [react(), tailwindcss()],
   define: {
     "process.env": JSON.stringify({}),
+    "process.stdout": JSON.stringify({}),
+    "process.stderr": JSON.stringify({}),
+    "process.platform": JSON.stringify("browser"),
     'process.argv': JSON.stringify([]),
   },
   resolve: {
